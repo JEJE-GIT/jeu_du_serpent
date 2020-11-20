d930e30e0e963c78376458c21fd10c7200bff35d
JEJE-GIT <70326222+JEJE-GIT@users.noreply.github.com>
2020-11-20 11:08:21 -0500

    La "Création du Jeu" a débuté

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //Le jeu
+
+    class Jeu{
+
+        constructor() {         //la fonction "constructor" sert a initier des variables pour la classe "Jeu"
+
+            console.log("Création du Jeu")
+
+        }
+
+    }
+
+
+    //Le serpent
+
+    class Serpent{
+
+        constructor() {         //la fonction "constructor" sert a initier des variables pour la classe "Serpent"
+
+            console.log("Création du Serpent")
+
+        }
+
+    }
+
+
+    //La pomme
+
+    class Pomme{
+
+        constructor() {         //la fonction "constructor" sert a initier des variables pour la classe "Serpent"
+
+            console.log("Création de la pomme")
+
+        }
+
+    }
+
+    var unePartie = new Jeu();
 
 });
